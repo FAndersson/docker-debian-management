82ea495d0272559a6f11f4509570798c3649a3f0
Fredrik Andersson <fredrik.andersson@industrialpathsolutions.com>
2023-11-15 21:37:59 +0100

    Add possibility to build from local tag.
    For experimenting with the image before pushing to GitHub and Docker
    hub.

--- docker_build_from_latest_tag.py
+++ docker_build_from_latest_tag.py
@@ -1,11 +1,14 @@
 import datetime
+import pathlib
 import subprocess
+import time
 from typing import Optional
 
+from git import Repo, TagReference
 import github
 
 
-def get_latest_tag(access_token: str, repo_name: str) -> Optional[str]:
+def get_latest_github_tag(access_token: str, repo_name: str) -> Optional[str]:
     """
     Get latest tag from a GitHub repository.
 
@@ -46,7 +49,40 @@ def get_latest_tag(access_token: str, repo_name: str) -> Optional[str]:
     return latest_tag.name
 
 
-def build_image_from_tag(github_login: str, repo_name: str, tag: str,
+def get_latest_local_tag(repo_name: str) -> Optional[str]:
+    """
+    Get latest tag from a local repository.
+
+    :param repo_name: Name of the repository.
+    :return: Tag name, or None if repo doesn't exist or if it has no
+        tags.
+    """
+    repo_path = pathlib.Path(__file__).resolve().parent.parent / repo_name
+    git_repo = Repo(repo_path)
+
+    # Get all tags in the repo
+    tags = git_repo.tags
+    if len(tags) == 0:
+        # No tags in repo
+        return None
+    # Get latest tag by sorting tags on commit date
+
+    def commit_date(tag: TagReference) -> datetime.datetime:
+        """
+        Get date and time when the commit corresponding to a tag was
+        created.
+
+        :param tag: Git tag.
+        :return: Date and time when the commit was created.
+        """
+        return datetime.datetime.fromtimestamp(
+            time.mktime(time.gmtime(tag.commit.committed_date)))
+
+    latest_tag = max(tags, key=commit_date)
+    return latest_tag
+
+
+def build_image_from_github_tag(github_login: str, repo_name: str, tag: str,
                          docker_login: str) -> Optional[str]:
     """
     Build a Docker image from a tag in a GitHub repository.
@@ -61,7 +97,7 @@ def build_image_from_tag(github_login: str, repo_name: str, tag: str,
     """
     assert repo_name[0:7] == "docker-"
     docker_image = repo_name[7:]
-    docker_tag = "{}/{}:{}".format(docker_login, docker_image, tag)
+    docker_tag = f"{docker_login}/{docker_image}:{tag}"
     repo_url = "https://github.com/{}/{}.git".format(github_login, repo_name)
     repo_url_with_tag = repo_url + "#{}".format(tag)
     build_command = [
@@ -79,6 +115,38 @@ def build_image_from_tag(github_login: str, repo_name: str, tag: str,
         return None
     print(f"Docker image {docker_tag} built from GitHub repo {repo_name}, "
           f"tag {tag}")
+    return docker_tag
+
+
+def build_image_from_local_tag(repo_name: str, tag: str,
+                               docker_login: str) -> Optional[str]:
+    """
+    Build a Docker image from a tag in a local repository.
+
+    :param repo_name: Name of the repository.
+    :param tag: Name of the tag to build from.
+    :param docker_login: Name of the Docker hub account the image should
+        be pushed to.
+    :return: Return tag for the new image, or None if the build failed.
+    """
+    assert repo_name[0:7] == "docker-"
+    docker_image = repo_name[7:]
+    docker_tag = f"{docker_login}/{docker_image}:{tag}"
+    repo_path = pathlib.Path(__file__).resolve().parent.parent / repo_name
+    build_command = [
+        "docker",
+        "build",
+        "--tag",
+        docker_tag,
+        str(repo_path)
+    ]
+    print("    Build command: " + " ".join(build_command))
+    result = subprocess.run(build_command, capture_output=True)
+    if result.returncode != 0:
+        print(f"docker build failed. stdout: {result.stdout}, stderr: "
+              f"{result.stderr}")
+        return None
+    print(f"Docker image {docker_tag} built from {repo_name}, tag {tag}")
     return docker_tag
 
 
@@ -153,9 +221,9 @@ def push_image_to_dockerhub(access_token: str, tag: str) -> bool:
     return True
 
 
-def build_and_push_tag(github_login: str, github_repo_name: str,
-                       github_repo_tag: str, docker_login: str,
-                       docker_access_token: str) -> bool:
+def build_and_push_github_tag(
+        github_login: str, github_repo_name: str, github_repo_tag: str,
+        docker_login: str, docker_access_token: Optional[str]) -> bool:
     """
     Build a Docker image from the Dockerfile in the root of a GitHub
     repository. The version of the repository corresponding to the
@@ -169,25 +237,32 @@ def build_and_push_tag(github_login: str, github_repo_name: str,
     :param docker_login: Name of the Docker hub account the image should
         be pushed to.
     :param docker_access_token: Personal access token to the Docker hub
-        account the image should be pushed to.
-    :return: Whether the image was successfully built and pushed
-        to Docker hub.
+        account the image should be pushed to. Will not push the built
+        image to Docker hub if not provided.
+    :return: Whether the image was successfully built and optionally
+        pushed to Docker hub.
     """
-    image_tag = build_image_from_tag(github_login, github_repo_name,
-                                     github_repo_tag, docker_login)
+    image_tag = build_image_from_github_tag(
+        github_login, github_repo_name, github_repo_tag, docker_login)
     if image_tag is None:
         return False
-    return push_image_to_dockerhub(docker_access_token, image_tag)
+    if docker_access_token:
+        return push_image_to_dockerhub(docker_access_token, image_tag)
+    return True
 
 
-def update_fa_repos(github_access_token, docker_access_token):
+def update_fa_repos(
+        github_access_token: Optional[str] = None,
+        docker_access_token: Optional[str] = None):
     """
-    Build and push Docker images for my private GitHub repos.
+    Build and optionally push Docker images for my private GitHub repos.
 
     :param github_access_token: Personal access token to the
-        'FAndersson' account.
+        'FAndersson' account. Will build from latest tag in GitHub if
+        provided. Otherwise build from latest local tag.
     :param docker_access_token: Personal access token to the
-        'fredrikandersson' account.
+        'fredrikandersson' account. Will push the built image to Docker
+        hub if provided.
     """
     github_login = "FAndersson"
     docker_login = "fredrikandersson"
@@ -206,14 +281,21 @@ def update_fa_repos(github_access_token, docker_access_token):
     ]
     for repo in repos:
         print(f"In repo {repo}")
-        latest_tag = get_latest_tag(github_access_token, repo)
+        if github_access_token:
+            latest_tag = get_latest_github_tag(github_access_token, repo)
+        else:
+            latest_tag = get_latest_local_tag(repo)
         if latest_tag is None:
             print(f"Failed to get latest tag in repo {repo}")
             return
-        if not build_and_push_tag(github_login, repo, latest_tag, docker_login,
-                                  docker_access_token):
-            print(f"Failed to build and push image for repo {repo}")
-            return
+        if github_access_token:
+            if not build_and_push_github_tag(
+                github_login, repo, latest_tag, docker_login,
+                docker_access_token):
+                print(f"Failed to build and push image for repo {repo}")
+        else:
+            if not build_image_from_local_tag(repo, latest_tag, docker_login):
+                print(f"Failed to build image for repo {repo}")
 
 
 if __name__ == "__main__":
